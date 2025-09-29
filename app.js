febbd59cb3f8ab4c56848e3e79b56dcea371d4ab
Shulabh Bhattarai <shulabhb@gmail.com>
2025-09-29 18:52:29 -0400

    Add missing auth routes import and registration

--- app.js
+++ app.js
@@ -21,12 +21,16 @@ const crypto = require("crypto");
 const TriviaCategory = require("./models/TriviaCategory");
 const UserActivity = require("./models/UserActivity");
 const User = require("./models/User");
+const authRoutes = require("./routes/auth");
 
 const app = express();
 
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
+
+// Routes
+app.use("/api/auth", authRoutes);
 
 // Database connection string: use env or default to original hosting URL
 const DEFAULT_MONGO_URI = "mongodb+srv://cognizennet:cognizennet@triviaquestions.gfew0.mongodb.net/?retryWrites=true&w=majority&appName=TriviaQuestions";
