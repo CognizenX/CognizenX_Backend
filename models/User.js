0ea4fbfd6c2497cbf2719b82f48d0b22d26a5abe
buikhueanh <anhbui_2026@depauw.edu>
2025-11-17 23:33:16 -0500

    Implement password reset

--- models/User.js
+++ models/User.js
@@ -4,7 +4,9 @@ const UserSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    sessionToken: { type: String, required: false }, 
+    // Password reset fields
+    resetPasswordToken: { type: String, required: false, index: true },
+    resetPasswordExpires: { type: Date, required: false },
 });
 
 // Encrypt password before saving
@@ -12,5 +14,21 @@ UserSchema.pre("save", async function (next) {
     if (!this.isModified("password")) return next();
     next();
 });
+
+// Instance helper: set a password reset token and expiry, returns the raw token
+UserSchema.methods.generatePasswordReset = function (ttlMs = 1000 * 60 * 60) {
+    const crypto = require("crypto");
+    const rawToken = crypto.randomBytes(32).toString("hex");
+    const hashed = crypto.createHash("sha256").update(rawToken).digest("hex");
+    this.resetPasswordToken = hashed;
+    this.resetPasswordExpires = new Date(Date.now() + ttlMs);
+    return rawToken;
+};
+
+// Instance helper: clear password reset fields
+UserSchema.methods.clearPasswordReset = function () {
+    this.resetPasswordToken = undefined;
+    this.resetPasswordExpires = undefined;
+};
 
 module.exports = mongoose.model("User", UserSchema);
