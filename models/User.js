46659c49293754d8fce65c76fe81b19f9700e237
buikhueanh <anhbui_2026@depauw.edu>
2026-03-01 16:55:58 -0500

    modify user fields

--- models/User.js
+++ models/User.js
@@ -3,7 +3,7 @@ const USER_CONSTRAINTS = require("../config/userConstraints");
 
 const UserSchema = new mongoose.Schema({
     name: { type: String, required: true },
-    email: { type: String, required: true, unique: true, trim: true, lowercase: true },
+    email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
 
     // Background Fields
